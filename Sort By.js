ef467f80559e81a5a777a4af3bf5f3e7ecb56477
aadil080 <aadilmohammad172@gmail.com>
2023-11-16 23:46:38 +0530

    Created Sort By.js

---
+++ Sort By.js
@@ -0,0 +1,96 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+
+const OptionComponent = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const options = [
+    {id: 1, icon: require('./assets/images/star.png'), title: 'Popular first'},
+    {
+      id: 2,
+      icon: require('./assets/images/calendar.png'),
+      title: 'Date posted',
+    },
+    {id: 3, icon: require('./assets/images/dollar-sign.png'), title: 'Price'},
+  ];
+
+  const handleSelectOption = optionId => {
+    // Toggle the selection state
+    setSelectedOption(prevSelected =>
+      prevSelected === optionId ? null : optionId,
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {options.map((option, index) => (
+        <View key={option.id} >
+          <TouchableOpacity
+            onPress={() => handleSelectOption(option.id)}
+            style={styles.optionContainer}
+            activeOpacity={0.8}>
+            <Image source={option.icon} style={styles.icon} />
+            <Text style={styles.title}>{option.title}</Text>
+            <View
+              style={[
+                styles.radioButton,
+                selectedOption === option.id && {backgroundColor: 'black'},
+              ]}>
+              {selectedOption === option.id && (
+                <View style={styles.radioButtonInner} />
+              )}
+            </View>
+          </TouchableOpacity>
+          {index !== options.length - 1 && <View style={styles.separator} />}
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    height: 'auto',
+    width: '90%',
+    borderWidth: 1,
+    borderRadius: 20,
+  },
+  optionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    width: '100%',
+    marginVertical: 20
+  },
+  icon: {
+    width: 28,
+    height: 28,
+    marginRight: 12,
+  },
+  title: {
+    flex: 1,
+    fontSize: 18,
+    color: 'black'
+  },
+  radioButton: {
+    width: 24,
+    height: 24,
+    borderWidth: 1,
+    borderRadius: 13,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  radioButtonInner: {
+    width: 9,
+    height: 9,
+    borderRadius: 6,
+    backgroundColor: '#ffffff',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC',
+  },
+});
+
+export default OptionComponent;
